4bff5330ac821de3563717820ca0f6730fa8f3aa
Gabriel <bielbuttendorf@gmail.com>
2026-03-05 21:20:50 -0300

    criação de formulário de login e cadastro

---
+++ src/pages/auth/sign-up.tsx
@@ -0,0 +1,113 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signUpForm = z.object({
+  restaurantName: z.string(),
+  managerName: z.string(),
+  phone: z.string(),
+  email: z.string().email(),
+})
+
+type SignUpForm = z.infer<typeof signUpForm>
+
+export function SignUp() {
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignUpForm>({
+    resolver: zodResolver(signUpForm),
+  })
+
+  async function handleSignUp(data: SignUpForm) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      toast.success('Estabelecimento cadastrado.', {
+        action: {
+          label: 'Login',
+          onClick: () => navigate('/sign-in'),
+        },
+      })
+      console.log(data)
+    } catch {
+      toast.error('Erro ao cadastrar estabelecimento.')
+    }
+  }
+
+  return (
+    <div>
+      <Helmet title="Cadastro" />
+      <div className="p-8">
+        <Button variant={'ghost'} asChild className="absolute top-4 right-4">
+          <Link to="/sign-in">Fazer login</Link>
+        </Button>
+
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Criar conta grátis
+            </h1>
+            <p className="text-muted-foreground text-sm">
+              Seja um parceiro e comece a vender!
+            </p>
+          </div>
+
+          <form onSubmit={handleSubmit(handleSignUp)} className="space-y-2">
+            <div className="space-y-2">
+              <Label htmlFor="restaurantName">Nome do estabelecimento</Label>
+              <Input
+                id="restaurantName"
+                type="text"
+                {...register('restaurantName')}
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="managerName">Seu nome</Label>
+              <Input
+                id="managerName"
+                type="text"
+                {...register('managerName')}
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="phone">Seu telefone</Label>
+              <Input id="phone" type="tel" {...register('phone')} />
+            </div>
+
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              Realizar cadastro
+            </Button>
+
+            <p className="text-muted-foreground px-6 text-center text-sm leading-relaxed">
+              Ao continuar você concorda com nossos{' '}
+              <a href="" className="underline underline-offset-4">
+                Termos de Serviço
+              </a>{' '}
+              e{' '}
+              <a href="" className="underline underline-offset-4">
+                Políticas de Privacidade
+              </a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
