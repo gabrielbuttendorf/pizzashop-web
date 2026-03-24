162d2bd30943255f3df28b1645eb9b6a758b1ced
Gabriel <bielbuttendorf@gmail.com>
2026-03-24 20:45:59 -0300

    criando tabela de pedidos

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
+import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 
@@ -11,6 +12,7 @@ export function AppRoutes() {
     <Routes>
       <Route element={<AppLayout />}>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/orders" element={<Orders />} />
       </Route>
 
       <Route element={<AuthLayout />}>
