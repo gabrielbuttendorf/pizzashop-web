be319a1c937debf6c4921ec7eb2fa26ae326bd69
Gabriel <bielbuttendorf@gmail.com>
2026-03-25 21:44:29 -0300

    criando paginação e modal com detalhe dos pedidos

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,3 +1,4 @@
+import { Pagination } from '@/components/pagination'
 import {
   Table,
   TableBody,
@@ -15,30 +16,31 @@ export function Orders() {
       <title>Cadastro | pizza.shop</title>
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <OrderTableFilters />
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow className="text-slate-400">
-                <TableHead className="w-16"></TableHead>
-                <TableHead className="w-35">Identificador</TableHead>
-                <TableHead className="w-45">Realizado há</TableHead>
-                <TableHead className="w-35">Status</TableHead>
-                <TableHead>Cliente</TableHead>
-                <TableHead className="w-35">Total do pedido</TableHead>
-                <TableHead className="w-41"></TableHead>
-                <TableHead className="w-33"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return <OrderTableRow key={i} />
-              })}
-            </TableBody>
-          </Table>
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow className="text-slate-400">
+                  <TableHead className="w-16"></TableHead>
+                  <TableHead className="w-35">Identificador</TableHead>
+                  <TableHead className="w-45">Realizado há</TableHead>
+                  <TableHead className="w-35">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-35">Total do pedido</TableHead>
+                  <TableHead className="w-41"></TableHead>
+                  <TableHead className="w-33"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return <OrderTableRow key={i} />
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
