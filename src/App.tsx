06c188844ddbd45c0b16d27636b3470208d876bb
Gabriel <bielbuttendorf@gmail.com>
2026-05-25 20:34:29 -0300

    configurando api client e react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 import { AppRoutes } from './routes'
 
 export function App() {
   return (
     <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
-      <BrowserRouter>
-        <Toaster richColors />
-        <AppRoutes />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Toaster richColors />
+          <AppRoutes />
+        </BrowserRouter>
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
